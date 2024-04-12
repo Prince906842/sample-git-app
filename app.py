073c9b2dd53daf974fe6fd21a960c92a075f8554
prince raj <jarvisraj04@gmail.com>
2024-04-12 18:57:59 +0530

    add course to home page

--- app.py
+++ app.py
@@ -3,10 +3,16 @@ st.title("campusx")
 
 st.image("unnamed.jpg")
 
-col1,col2= st.columns(col1,col2)
+col1,col2= st.columns(2)
 
 with col1:
     st.image("unnamed.jpg")
 
 with col2:
     st.header("campusX on the platform")
+
+st.header("Course")
+st.subheader("DSMP1")
+st.subheader("DSM2")
+st.subheader("DSM3")
+st.subheader("DSM4")
