76aa07c4cbc994c5aecc1fad103ec209c9c3505a
prince raj <jarvisraj04@gmail.com>
2024-04-12 18:52:00 +0530

    add image and heading on home page

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+st.title("campusx")
+
+st.image("unnamed.jpg")
+
+col1,col2= st.columns(col1,col2)
+
+with col1:
+    st.image("unnamed.jpg")
+
+with col2:
+    st.header("campusX on the platform")
